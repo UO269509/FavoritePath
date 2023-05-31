48537312e1dc6464a7ee47a056908ae53b09d9d1
UO269509 <uo269509@uniovi.es>
2023-05-31 14:59:50 +0200

    APK

---
+++ app/build/generated/source/buildConfig/debug/uo269509/favoritepath/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package uo269509.favoritepath;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "uo269509.favoritepath";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
